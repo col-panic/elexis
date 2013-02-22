a600c899bdb996c00b2cecde99eb579d5c990270
Marco Descher <marco@descher.at>
2013-02-22 10:14:26 +0100

    [1177] Initial check-in ATC code service

---
+++ at.medevit.atc_codes/src/at/medevit/atc_codes/internal/Activator.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2013 MEDEVIT.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     MEDEVIT <office@medevit.at> - initial API and implementation
+ ******************************************************************************/
+package at.medevit.atc_codes.internal;
+
+import java.io.File;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+	
+	private static BundleContext ctx;
+	
+	@Override
+	public void start(BundleContext context) throws Exception{
+		this.ctx = context;
+	}
+	
+	@Override
+	public void stop(BundleContext context) throws Exception{
+		ctx = null;
+	}
+	
+	public static File getATCFile(){
+		return ctx.getDataFile("rsc/2013ATC.XML");
+	}
+	
+	public static File getATC_DDDFile(){
+		return ctx.getDataFile("rsc/2013ATC_ddd.xml");
+	}
+	
+}
