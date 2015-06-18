292395e0c39e47ea6596fe4935dab7e4dd1bd996
Marco Descher <marco@descher.at>
2015-06-18 11:03:20 +0200

    Extend HashMapBlobLoader to consider Lists and statL objects

--- at.medevit.elexisHelpers/src/at/medevit/elexisHelpers/HashMapBlobLoader.java
+++ at.medevit.elexisHelpers/src/at/medevit/elexisHelpers/HashMapBlobLoader.java
@@ -3,9 +3,13 @@ package at.medevit.elexisHelpers;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.lang.reflect.Field;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Set;
 import java.util.zip.ZipInputStream;
+
+import ch.elexis.data.Kontakt.statL;
 
 public class HashMapBlobLoader {
 	public static void main(String[] args) throws IOException, ClassNotFoundException{
@@ -24,7 +28,32 @@ public class HashMapBlobLoader {
 		Hashtable ht = (Hashtable) readObject;
 		Set keySet = ht.keySet();
 		for (Object object : keySet) {
-			System.out.println("[" + object + "] " + ht.get(object));
+			System.out.println("[" + object + "]");
+			String val = ts(ht.get(object));
+			if(val!=null) {
+				System.out.println("\t "+ val);
+			}
 		}
+	}
+
+	private static String ts(Object object){
+		if(object instanceof List) {
+			List l = (List) object;
+			for (int i = 0; i < l.size(); i++) {
+				Object o = l.get(i);
+				System.out.println("\t "+i+"/"+l.size()+": "+ts(o));
+			}
+			return null;
+		} else if (object instanceof statL) {
+			statL s = (statL) object;
+			try {
+				Field declaredField = statL.class.getDeclaredField("v");
+				declaredField.setAccessible(true);
+				return statL.class.getName()+" "+declaredField.get(s);
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				return statL.class.getName() +" "+e.getMessage();
+			} 
+		}
+		return object.toString();
 	}
 }
