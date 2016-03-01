9e06f0587260ab93fc356cad1aa70746cb2b492e
Marco Descher <marco@descher.at>
2016-03-01 12:43:40 +0100

    Remove old Artikelstamm converter bundles

--- net.swissindex.format/src-gen/net/swissindex/ws/nonpharma/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://swissindex.e-mediat.net/SwissindexNonPharma_out_V101", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package net.swissindex.ws.nonpharma;
